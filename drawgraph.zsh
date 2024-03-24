d1a50625d46609da91d2717ad40ff40c6044f24d
frederik <frederik.p.stender@gmail.com>
2024-03-24 09:16:54 +0100

    restructure project

--- drawgraph.zsh
+++ drawgraph.zsh
@@ -1,2 +1,3 @@
-javac drawgraph.java
-java drawgraph < samples/$1.in ; dot -Tpng harmony.txt -Gsize=20,20\! -Gdpi=100 -o harmony$1.png
+# use: zsh drawgraph.zsh 1.in 
+# javac drawgraph.java
+java drawgraph < input/$1 ; dot -Tpng output/$1.txt -Gsize=20,20\! -Gdpi=100 -o output/$1.png
